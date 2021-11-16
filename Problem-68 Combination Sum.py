316edb061cfd811395b63fb9142ee79e8a72f5da
Adhish Shrivastava <adhish70@gmail.com>
2021-11-16 10:24:08 -0500

    Problem68 done

--- Problem-68 Combination Sum.py
+++ Problem-68 Combination Sum.py
@@ -1,11 +1,37 @@
 # 39. Combination Sum
 # https://leetcode.com/problems/combination-sum/
 
-# Logic:
+# Time Complexiety: O(n*(2^n))
+# Space Complexiety: O(n)
 
-# Time Complexiety: 
-# Space Complexiety: 
-
-class Solution:     
+class Solution:
+    def __init__(self):
+        self.result = list()
+        
+    def _helper(self, candidates, target, idx, cur, path):
+        # Base condition
+        if idx == len(candidates):
+            return
+        
+        if cur > target:
+            return
+        
+        if cur == target:
+            self.result.append(path.copy())
+        
+        n = len(candidates)
+        for i in range(idx, n):
+            # action
+            path.append(candidates[i])
+            cur += candidates[i]
+            
+            # recursion
+            self._helper(candidates, target, i, cur, path)
+            
+            # backtrack
+            cur -= candidates[i]
+            path.pop()
+        
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+        self._helper(candidates, target, 0, 0, [])
+        return self.result
